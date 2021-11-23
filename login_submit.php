6b928f8910f441dab9173c6667a000cef84a00c5
premsharma2206 <premsharma2206@gmail.com>
2021-11-23 13:23:32 -0600

    initial commit

--- login_submit.php
+++ login_submit.php
@@ -1,14 +1,14 @@
 <?php
 
 require("includes/common.php");
-
-$username = $_POST['username'];
-$username = mysqli_real_escape_string($con, $username);
+$con = $mysqli;
+$email = $_POST['email'];
+$email = mysqli_real_escape_string($con, $email);
 $password = $_POST['password'];
 $password = mysqli_real_escape_string($con, $password);
-$password = MD5($password);
+// $password = MD5($password);
 // Query checks if the email and password are present in the database.
-$query = "SELECT id, email FROM users WHERE email='" . $email . "' AND password='" . $password . "'";
+$query = "SELECT member_ID, email FROM member WHERE email='" . $email . "' AND password='" . $password . "'";
 $result = mysqli_query($con, $query)or die($mysqli_error($con));
 $num = mysqli_num_rows($result);
 // If the email and password are not present in the database, the mysqli_num_rows returns 0, it is assigned to $num.
@@ -17,12 +17,12 @@ if ($num == 0) {
   header('location: login.php?error=' . $error);
 } else {  
   $row = mysqli_fetch_array($result);
-  $_SESSION['username'] = $row['username'];
-  $_SESSION['user_id'] = $row['id'];
-   if($_SESSION['username']=="premsharma22")
+  $_SESSION['email'] = $row['email'];
+  $_SESSION['memberid'] = $row['member_ID'];
+   if($_SESSION['email']=="premsharma22")
         header('location: admin/index.php');
     else {
-        header('location: products.php');
+        header('location: Items_list.php');
     }
 }
 ?>
