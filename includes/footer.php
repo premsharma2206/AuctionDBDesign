38b91250b2748cf91e70fc80dadf1895d88a034f
Prem Sharma <premsharma2206@gmail.com>
2021-11-23 12:25:24 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>  Contact Us: +1</p>	
+        </center>
+    </div>
+</footer>
---
+++ js/jQuery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$.ajax({
+		url: "",
+		dataType: "json",
+		success: function(data){
+
+ 	},
+ 	error: function() { alert("Error loading JSON file");  },
+ });
+});
+
+
+
+
+
